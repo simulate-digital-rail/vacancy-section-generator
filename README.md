0e3abb0e7615afe1f6182b7548c98117342e9b0c
Clemens Tiedt <clemens.tiedt@student.hpi.de>
2023-02-14 17:25:53 +0100

    implement basic vacancy section generation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # vacancy-section-generator
-Generate vacancy sections for yaramo topologies
+
+Generate vacancy sections (Freimeldeabschnitte) for yaramo topologies
+
+## Process
+
+This generator works on the assumption that one edge can be part of zero or one vacancy sections.
+It will create one vacancy section for each edge and also add them to the topology as a whole as
+well as the relevant routes.
+
+## Installation
+```shell
+$ poetry add git+https://github.com/simulate-digital-rail/yaramo
+$ poetry install
+```
+
+## Usage
+
+```python
+VacancySectionGenerator(topology).generate()
+print(topology.vacancy_sections)
+```
---
+++ vacancy_section_generator/main.py
@@ -0,0 +1,18 @@
+from yaramo.model import Topology
+from yaramo.vacancy_section import VacancySection
+
+class VacancySectionGenerator:
+    def __init__(self, topology: Topology) -> VacancySectionGenerator:
+        self.topology = topology
+
+    def generate(self):
+        for edge in self.topology.edges.values():
+            if not edge.vacancy_section:
+                vacancy_section = VacancySection()
+                edge.vacancy_section = vacancy_section
+                self.topology.add_vacancy_section(vacancy_section)
+        
+        for route in self.topology.routes.values():
+            for edge in route.edges:
+                if edge.vacancy_section:
+                    route.vacancy_sections.add(edge.vacancy_section)
